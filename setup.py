77cabc1bbbe18d4a9082e42158713f0f90feedf5
vmmadathil <madathilvisakh@gmail.com>
2020-12-28 15:21:57 -0600

    intital commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This will be a real-time economic activity tracker. It will be a central repository for economic data, visualizations, and analysis.',
+    author='Visakh Madathil',
+    license='MIT',
+)
